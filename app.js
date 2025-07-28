c38c16190e428ed107a24e66bc6a83d81e2c295a
Aman <amanfarhan2008@gmail.com>
2025-07-28 22:57:55 +0500

    otp

--- app.js
+++ app.js
@@ -7,3 +7,10 @@
 // }
 // let a = Math.floor(coin) ;
 // console.log(a);
+
+// let otp = Math.floor(Math.random() *999 + 1000);
+// console.log(otp);
+
+
+// let otp = Math.floor(Math.random() *999 + 2000);
+// console.log(otp);
