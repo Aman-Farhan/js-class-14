290ce8f8814586ac6e83998e6f33b1ab3944aabd
Aman <amanfarhan2008@gmail.com>
2025-07-28 22:40:15 +0500

    fg

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 let coin = Math.random();
+    if (coin < 0.5 ){
+        console.log("Heads" , coin);
+        
+} else {
+        console.log(coin);
 
-let a = Math.floor(coin) ;
-console.log(a);
+}
+// let a = Math.floor(coin) ;
+// console.log(a);
