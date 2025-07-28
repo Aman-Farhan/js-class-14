a8251a3c83d813cb07b782fef67a9731dbf09262
Aman <amanfarhan2008@gmail.com>
2025-07-29 00:00:07 +0500

    if else prectice

--- app.js
+++ app.js
@@ -21,6 +21,41 @@
 // let a = Math.floor(otp) *999 + 1000;
 // console.log(a);
 
-let a = Math.random();
-let b = Math.ceil(a) * 5 + 5;
-console.log(Math.sqrt(b,2));
+// let a = Math.random();
+// let b = Math.ceil(a) * 5 + 5;
+// console.log(Math.sqrt(b,2));
+
+
+// let a = +prompt("Enter A Number ");
+// let b = +prompt("Enter A Number");
+// let c = +prompt("Enter A Number");
+
+// if (a > b && a > c){
+//     console.log("A is greater")
+// } 
+// else if (b > a && b > c){
+//     console.log("B is greater")
+// } 
+// else{
+//     console.log("C is greater")
+
+// }
+
+
+
+let a = +prompt("Enter A Number ");
+let b = +prompt("Enter A Number");
+let c = +prompt("Enter A Number");
+
+if (a > b && a > c){
+    console.log("A is Greater");
+}
+else if (b > a && b > c){
+    console.log("B is Greater");
+}
+else{
+    console.log("C is Greater");
+}
+
+
+
